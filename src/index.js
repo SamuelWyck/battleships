c5b6a9feccf0fdb90cd682e8ba22bb089e8599e4
samuel <samawyck@gmail.com>
2025-04-02 15:47:23 -0500

    Link player object logic to GUI

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const game = (function() {
         if (!gameStarted) {
             event.dataTransfer.clearData();
             event.dataTransfer.setData("text", event.target.id);
+            event.target.classList.add("lower-element");
         }
     };
 
@@ -29,19 +30,29 @@ const game = (function() {
     function shipDrop(event) {
         event.preventDefault();
         if (!gameStarted) {
-            if (event.target.matches(".ship") || event.target.matches(".board-coord-cell")) {
+            const shipId = event.dataTransfer.getData("text");
+            const ship = document.getElementById(shipId);
+            ship.classList.remove("lower-element");
+
+            if (event.target.matches(".board-coord-cell")) {
                 return;
             }
 
-            const shipId = event.dataTransfer.getData("text");
-
             if (event.target.matches(".ship-container")) {
-                const ship = document.getElementById(shipId);
                 ship.classList.remove("horizontal");
+                removeShip(ship);
                 event.target.appendChild(ship);
             } else {
-                const target = (event.target.classList.contains("board-cell-marker")) ? event.target.parentNode : event.target;
-                target.appendChild(document.getElementById(shipId));
+                const target = (event.target.classList.contains("ship")) ? getLowerElement(event) : event.target;
+                const shipData = saveShipData(ship);
+                removeShip(ship);
+                const shipPlaced = placeShip(target, ship);
+                if (shipPlaced) {
+                    target.appendChild(ship);
+                } else {
+                    replaceShip(shipData);
+                }
+                console.log(playerOne.board.board);
             }
         }
     };
@@ -49,7 +60,70 @@ const game = (function() {
 
     function shipRotate(event) {
         if (!gameStarted) {
-            event.target.classList.toggle("horizontal");
+            const shipData = saveShipData(event.target);
+            removeShip(event.target);
+            const shipPlaced = playerOne.placeShip(
+                shipData.length, 
+                shipData.row,
+                shipData.col, 
+                !shipData.horizontal
+            );
+            if (shipPlaced) {
+                event.target.classList.toggle("horizontal");
+            } else {
+                playerOne.placeShip(
+                    shipData.length, 
+                    shipData.row,
+                    shipData.col, 
+                    shipData.horizontal
+                );
+            }
         }
-    }
+    };
+
+
+    function getLowerElement(event) {
+        const x = event.clientX;
+        const y = event.clientY;
+        const elements = document.elementsFromPoint(x, y);
+        return elements[1];
+    };
+
+
+    function saveShipData(ship) {
+        const length = Number(ship.dataset.length);
+        const row = Number(ship.parentNode.dataset.row);
+        const col = Number(ship.parentNode.dataset.col);
+        const horizontal = ship.classList.contains("horizontal");
+        return {"row": row, "col": col, "length": length, "horizontal": horizontal};
+    };
+
+
+    function placeShip(target, ship) {
+        const row = Number(target.dataset.row);
+        const col = Number(target.dataset.col);
+        const length = Number(ship.dataset.length);
+        const horizontal = ship.classList.contains("horizontal");
+        return playerOne.placeShip(length, row, col, horizontal);
+    };
+
+
+    function replaceShip(shipData) {
+        playerOne.placeShip(
+            shipData.length, 
+            shipData.row, 
+            shipData.col, 
+            shipData.horizontal
+        );
+    };
+
+
+    function removeShip(ship) {
+        if (ship.parentNode.classList.contains("ship-container")) {
+            return;
+        }
+        const row = Number(ship.parentNode.dataset.row);
+        const col = Number(ship.parentNode.dataset.col);
+        playerOne.removeShip(row, col);
+    };
 })();
