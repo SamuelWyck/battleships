893db7940696fcad980076f4657747eb555495c4
samuel <samawyck@gmail.com>
2025-04-04 12:26:16 -0500

    Create popup class

---
+++ src/interfaceParts/popup.js
@@ -0,0 +1,38 @@
+
+
+class PopUp {
+
+    constructor(popupClass, hideClass) {
+        this.parent = document.querySelector("body");
+        this.popup = document.createElement("span");
+        this.popup.classList.add(popupClass);
+        this.popup.classList.add(hideClass);
+        this.hideClass = hideClass;
+    };
+
+
+    createPopup() {
+        this.parent.appendChild(this.popup);
+    };
+
+
+    showPopup(text=null) {
+        if (!this.popup.classList.contains(this.hideClass)) {
+            return false;
+        }
+        if (text !== null) {
+            this.popup.textContent = text;
+        }
+
+        this.popup.classList.remove(this.hideClass);
+        return true;
+    };
+
+
+    hidePopup() {
+        this.popup.classList.add(this.hideClass);
+    };
+};
+
+
+export default PopUp;
