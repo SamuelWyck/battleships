723d658fab12e5c2fc2e4b66b64185d8f22a64a5
samuel <samawyck@gmail.com>
2025-04-07 16:14:15 -0500

    Change board attack return

--- src/gameParts/board.js
+++ src/gameParts/board.js
@@ -110,23 +110,23 @@ class Board {
         const rowValid = 0 <= row && row < this.#board.length;
         const colValid = 0 <= col && col < this.#board[0].length;
         if (!rowValid || !colValid) {
-            return [null, null, null];
+            return [null, null];
         }
         const key = JSON.stringify([row, col]);
         if (this.#attackedPositions.has(key)) {
-            return [null, null, null];
+            return [null, null];
         }
 
         this.#attackedPositions.add(key);
         const position = this.#board[row][col];
         if (typeof position === "number") {
             this.#board[row][col] = this.#missSymbol;
-            return [false, false, null];
+            return [false, false];
         }
 
         const ship = position;
         ship.hit();
-        return [true, ship.isSunk(), ship];
+        return [true, ship.isSunk()];
     };
 
 
