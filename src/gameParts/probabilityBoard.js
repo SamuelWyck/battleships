33fafc27901b2e96667d0bad60f6e771c888d118
samuel <samawyck@gmail.com>
2025-04-07 14:09:31 -0500

    Create probabilityBoard class

---
+++ src/gameParts/probabilityBoard.js
@@ -0,0 +1,146 @@
+import BoardCell from "./probabilityBoardCell";
+
+
+class ProbabilityBoard {
+
+    constructor() {
+        this.board = [
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+        ];
+    };
+
+
+    #cell() {
+        return new BoardCell();
+    };
+
+
+    recordAttack(row, col, hit) {
+        const rowValid = 0 <= row && row < this.board.length;
+        const colValid = 0 <= col && col < this.board[0].length;
+        if (!rowValid || !colValid) {
+            return false;
+        }
+
+        const cell = this.board[row][col];
+        if (hit) {
+            cell.hit = true;
+        } else {
+            cell.miss = true;
+        }
+        return true;
+    };
+
+
+    clearBoard() {
+        this.board = [
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+            [this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell(), this.#cell()],
+        ];
+    };
+
+
+    resetProbability() {
+        for (let row = 0; row < this.board.length; row += 1) {
+            for (let col = 0; col < this.board[0].length; col += 1) {
+                const cell = this.board[row][col];
+                cell.resetHuntWeight();
+            }
+        }
+    };
+
+
+    calculateProbability(shipList) {
+        for (let ship of shipList) {
+            this.#calcHorizontalProbability(ship);
+            this.#calcVerticalProbability(ship);
+        }
+    };
+
+
+    #calcHorizontalProbability(shipLength) {
+        const posChange = {"rowChange": 0, "colChange": 1};
+        for (let row = 0; row < this.board.length; row += 1) {
+            for (let col = 0; col <= this.board[row].length - shipLength; col += 1) {
+                this.#calcWeight(row, col, 1, shipLength, posChange);
+            }
+        }
+    };
+
+
+    #calcVerticalProbability(shipLength) {
+        const posChange = {"rowChange": 1, "colChange": 0};
+        for (let row = 0; row <= this.board.length - shipLength; row += 1) {
+            for (let col = 0; col < this.board[0].length; col += 1) {
+                this.#calcWeight(row, col, 1, shipLength, posChange);
+            }
+        }
+    };
+
+
+    #calcWeight(row, col, currentLength, targetlength, posChange) {
+        const rowValid = 0 <= row && row < this.board.length;
+        const colValid = 0 <= col && col < this.board[0].length;
+        if (!rowValid || !colValid) {
+            return false;
+        }
+
+        const cell = this.board[row][col];
+        if (cell.hit || cell.miss) {
+            return false;
+        }
+        if (currentLength === targetlength) {
+            cell.increaseHuntWeight();
+            return true;
+        }
+
+        const newRow = row + posChange.rowChange;
+        const newCol = col + posChange.colChange;
+        const success = this.#calcWeight(newRow, newCol, currentLength + 1, targetlength, posChange);
+        if (success) {
+            cell.increaseHuntWeight();
+        }
+        return success;
+    };
+
+
+    getBestAttacksList() {
+        let attackList = [];
+        let maxWeight = 1;
+
+        for (let row = 0; row < this.board.length; row += 1) {
+            for (let col = 0; col < this.board[0].length; col += 1) {
+                const cell = this.board[row][col];
+                if (cell.huntWeight > maxWeight) {
+                    maxWeight = cell.huntWeight;
+                    attackList = [];
+                    attackList.push({"row": row, "col": col});
+                } else if (cell.huntWeight === maxWeight) {
+                    attackList.push({"row": row, "col": col});
+                }
+            }
+        }
+
+        return attackList;
+    };
+};
+
+
+export default ProbabilityBoard;
