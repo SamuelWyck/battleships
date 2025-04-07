9aa72e9b363cf7470f2bd8f699d816dbc6ba0ea0
samuel <samawyck@gmail.com>
2025-04-07 09:35:54 -0500

    Add method to randomly place ships on the player board

--- src/gameParts/player.js
+++ src/gameParts/player.js
@@ -54,6 +54,34 @@ class Player {
         }
     };
 
+
+    placeShips() {
+        while (this.shipList.length > 0) {
+            const ship = this.shipList.pop();
+            this.#placeShip(ship);
+        }
+    };
+
+
+    #placeShip(ship) {
+        while (true) {
+            const horizontal = this.#randInt(1, 10) <= 5;
+            const finalRow = (horizontal) ? this.board.board.length - 1 : this.board.board.length - ship;
+            const finalCol = (horizontal) ? this.board.board[0].length - ship : this.board.board[0].length - 1;
+            const row = this.#randInt(0, finalRow);
+            const col = this.#randInt(0, finalCol);
+            if (this.board.placeShip(ship, row, col, horizontal)) {
+                break;
+            }
+        }
+    };
+
+
+    #randInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 };
 
 
